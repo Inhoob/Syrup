0d89c5ed309e9c94f25e44bc074b4a5772741814
Inhoob <inhoob@gmail.com>
2023-03-03 00:39:18 +0900

    [STYLE] : COLORS 작성

---
+++ src/styles/colors.ts
@@ -0,0 +1,12 @@
+export default {
+  white: '#FFFFFF',
+  black: '#000000',
+  pastalRed: '#f64c3f',
+  veryLightPink: '#ffddda',
+  coral: '#f65f53',
+  greyishBrown: '#3f3f3f',
+  pinkishGrey: '#cecece',
+  beigh: '#f9dedd',
+  coralTwo: '#f95f53',
+  warmGrey: '#9e9e9e',
+};
